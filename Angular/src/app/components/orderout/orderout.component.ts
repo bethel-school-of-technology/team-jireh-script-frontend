6990aec34e4911f0075c9851b46879918a50e05d
Noah Zucker <njzucker@gmail.com>
2022-05-02 09:13:08 -0400

    commit

---
+++ Angular/src/app/components/orderout/orderout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orderout',
+  templateUrl: './orderout.component.html',
+  styleUrls: ['./orderout.component.css']
+})
+export class OrderoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
